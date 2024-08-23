0a572e5d29e861c21b23e26e2c3d97ae42919698
M B ABHIJITH NAIR <119162389+M-B-abhijith@users.noreply.github.com>
2024-08-23 07:29:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// const phrases = ["Web Developer", "UI/UX Designer", "Data Science Enthusiast"];
+const phrases = ["Web Developer", "UI/UX Designer", "Btech Student"];
+
+    let currentPhrase = 0;
+    let currentChar = 0;
+    const textElement = document.getElementById('animated-text');
+
+    function updateText() {
+        textElement.innerHTML = phrases[currentPhrase].substring(0, currentChar);
+        if (currentChar < phrases[currentPhrase].length) {
+            currentChar++;
+            setTimeout(updateText, 225); // Adjust the speed of typing
+        } else {
+            setTimeout(changePhrase, 1200); // Wait for 2 seconds before changing the phrase
+        }
+    }
+
+    function changePhrase() {
+        currentChar = 1;
+        currentPhrase = (currentPhrase + 1) % phrases.length;
+        setTimeout(updateText, 500); // Delay before starting to type the next phrase
+    }
+
+    updateText(); // Start the animation
+
+  
+    function scrollToSection(id) {
+        document.getElementById(id).scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+
+
+    function toggleMenu() {
+        const menu = document.querySelector('.menu');
+        const menuIcon = document.querySelector('.menu-icon');
+    
+        menu.classList.toggle('active');
+        menuIcon.classList.toggle('active');
+    }
+    
+
+
+ 
